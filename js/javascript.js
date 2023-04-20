f819e785293ac6517886bbaf9273fd5c9c408651
ansel <ansel.yu@gmail.com>
2023-04-20 15:58:00 +0300

    update

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,15 @@
 
+var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+});
 
 
+// let map = L.map('map').setView([58.373523, 26.716045], 12)
 
-let map = L.map('map').setView([58.373523, 26.716045], 12)
+// const osm = 
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: 'OpenStreetMap contributors', })
 
-const osm = 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: 'OpenStreetMap contributors', })
-
-    osm.addTo(map)
+//     osm.addTo(map)

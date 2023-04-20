cf2dedf9caae94ee09c79fc9ec112507937ba739
ansel <ansel.yu@gmail.com>
2023-04-20 15:46:59 +0300

    update

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,12 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
 
 
 
-// let map = L.map('map').setView([58.373523, 26.716045], 12)
 
-// const osm = 
-//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: 'OpenStreetMap contributors', })
+let map = L.map('map').setView([58.373523, 26.716045], 12)
 
-//     osm.addTo(map)
+const osm = 
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'OpenStreetMap contributors', })
+
+    osm.addTo(map)

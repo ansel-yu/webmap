1add75668d9bb318fbc9932d908326b3457b9f84
ansel <ansel.yu@gmail.com>
2023-05-06 15:37:48 +0300

    u

--- js/chomap.js
+++ js/chomap.js
@@ -7,27 +7,46 @@ const osm =
 
     osm.addTo(map)
 
-async function addGeoJson(url) {
-    const response = await fetch(url)
-    const data = await response.json()
-    L.choropleth(data, {
-        valueProperty: 'OBJECTID', // which property in the features to use
-        scale: ['white', 'red'], // chroma.js scale - include as many as you like
-        steps: 5, // number of breaks or steps in range
-        mode: 'q', // q for quantile, e for equidistant, k for k-means
-        style: {
-            color: '#fff', // border color
-            weight: 2,
-            fillOpacity: 0.8
-        },
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.OBJECTID)
-        }
-    })
-    L.choropleth.addTo(map)
+// async function addGeoJson(url) {
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     L.choropleth(data, {
+//         valueProperty: 'OBJECTID', // which property in the features to use
+//         scale: ['white', 'red'], // chroma.js scale - include as many as you like
+//         steps: 5, // number of breaks or steps in range
+//         mode: 'q', // q for quantile, e for equidistant, k for k-means
+//         style: {
+//             color: '#fff', // border color
+//             weight: 2,
+//             fillOpacity: 0.8
+//         },
+//         onEachFeature: function(feature, layer) {
+//             layer.bindPopup(feature.properties.OBJECTID)
+//         }
+//     }).addTo(map)
     
     
-}
+// }
 
 
-addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+// addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+
+$.getJSON('geojson/tartu_city_celltowers_edu.geojson', function (geojson) {
+    L.choropleth(geojson, {
+      valueProperty: 'OBJECTID',
+      scale: ['white', 'red'],
+      steps: 5,
+      mode: 'q',
+      style: {
+        color: '#fff',
+        weight: 2,
+        fillOpacity: 0.8
+      },
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup('District ' + feature.properties.dist_num + '<br>' +
+            feature.properties.incidents.toLocaleString() + ' incidents')
+      }
+    }).addTo(map)
+  })
+  

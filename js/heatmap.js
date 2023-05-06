d87626cbf65da9cb7d53d91f12e102867ee4224e
ansel <ansel.yu@gmail.com>
2023-05-06 13:56:25 +0300

    add heatmap

---
+++ js/heatmap.js
@@ -0,0 +1,20 @@
+addGeoJson('/geojson/tartu_city_celltowers_edu.geojson')
+
+async function addGeoJson(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    const heatData = data.features.map(heatDataConvert)
+    const heatMap = L.heatLayer(heatData, {
+        radius: 10
+    })
+    heatMap.addto(map)
+    // console.log(heatData)
+}
+
+function heatDataConvert(feature) {
+    return [
+        feature.geometry.coordinate[1],
+        feature.geometry.coordinate[0],
+        feature.properities.area,
+    ]
+}

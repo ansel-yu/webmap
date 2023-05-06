4c95ea2cf501fc0d62921367ade01a399e2e3275
ansel <ansel.yu@gmail.com>
2023-05-06 19:50:29 +0300

    u

--- js/chomap.js
+++ js/chomap.js
@@ -22,7 +22,7 @@ async function addGeoJson(url) {
             fillOpacity: 0.8
         },
         onEachFeature: function(feature, layer) {
-            layer.bindPopup(feature.properties.OBJECTID)
+            layer.bindPopup(feature.properties.area)
         }
     }).addTo(map)   
 }
